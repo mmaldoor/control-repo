87b18e77ac8545ca45c154c4bd588fe9f950ba48
Mustafa Aldoori <vnight95@gmail.com>
2022-10-21 16:35:01 +0200

    archive test

--- site-modules/snort/manifests/install.pp
+++ site-modules/snort/manifests/install.pp
@@ -10,18 +10,22 @@ class snort::install {
   }
 
 
-  if $facts['osfamily'] != 'windows' {
-    package { 'wget':
-      ensure => present,
-    }
+  # if $facts['osfamily'] != 'windows' {
+  #   package { 'wget':
+  #     ensure => present,
+  #   }
 
-    package { 'bunzip':
-      ensure => present,
-    }
+  #   package { 'bunzip':
+  #     ensure => present,
+  #   }
 
-    Archive {
-      provider => 'wget',
-      require  => Package['wget', 'bunzip'],
-    }
-  }
+  #   Archive {
+  #     provider => 'wget',
+  #     require  => Package['wget', 'bunzip'],
+  #   }
+  # }
+
+  Archive {
+  provider => 'ruby',
+}
 }
