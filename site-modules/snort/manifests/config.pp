18849a6a07e819f3069b1e3685be954c00da6e89
Mustafa Aldoori <vnight95@gmail.com>
2022-10-28 17:45:56 +0200

    ffs

--- site-modules/snort/manifests/config.pp
+++ site-modules/snort/manifests/config.pp
@@ -20,13 +20,13 @@ class snort::config (
 #
 #
 
-include 'archive'
+# include 'archive'
 
-archive { '/tmp/snortrules-snapshot-2983.tar.gz':
-  ensure        => present,
-  extract       => true,
-  extract_path  => '/etc/snort/rules',
-  checksum      => 'd0431bd39e082f8c1acd5c5c6df41980',
-  checksum_type => 'md5',
-  source        => 'https://www.snort.org/rules/snortrules-snapshot-2983.tar.gz?oinkcode=edbd39c3beb231a12ecf24e55ac03d873878bab4',
-}
+# archive { '/tmp/snortrules-snapshot-2983.tar.gz':
+#   ensure        => present,
+#   extract       => true,
+#   extract_path  => '/etc/snort/rules',
+#   checksum      => 'd0431bd39e082f8c1acd5c5c6df41980',
+#   checksum_type => 'md5',
+#   source        => 'https://www.snort.org/rules/snortrules-snapshot-2983.tar.gz?oinkcode=edbd39c3beb231a12ecf24e55ac03d873878bab4',
+# }
