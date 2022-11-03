ab74f69ccc14716d77e8f8438feef91b85f53e4d
Mustafa Aldoori <vnight95@gmail.com>
2022-11-03 23:55:55 +0100

    ffs

--- site-modules/snort/manifests/install.pp
+++ site-modules/snort/manifests/install.pp
@@ -19,6 +19,6 @@ class snort::install {
   package { 'snort':
     ensure          => 'present',
     provider        => 'apt',
-    install_options => ['ens3', '192.168.180.0/24', 'ens3'],
+    # install_options => ['ens3', '192.168.180.0/24', 'ens3'],
   }
 }
