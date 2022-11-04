614ad029e4fd0bc3531d432bcb9d481fb8d6c001
Mustafa Aldoori <vnight95@gmail.com>
2022-11-04 13:20:34 +0100

    hiera

--- site-modules/snort/manifests/config.pp
+++ site-modules/snort/manifests/config.pp
@@ -5,8 +5,8 @@
 # @example
 #   include snort::config
 class snort::config (
-  $ip_range = '192.168.180.0/24',  #defaults
-  $interface = 'ens3',
+  Optional $ip_range,
+  Optional $interface,
 ) {
   $snort_conf_hash = {
     'ip_range' => $ip_range,
